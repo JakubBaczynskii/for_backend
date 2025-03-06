f95def0a191227b61dd95fd4dda2f47a9bbd7524
Jakub Baczyński <124045523+JakubBaczynskii@users.noreply.github.com>
2025-03-06 12:37:08 +0100

    Created a colored square

--- src/App.js
+++ src/App.js
@@ -1,64 +1,88 @@
-import Header from './Header';
-import SearchItem from './SearchItem';
-import AddItem from './AddItem';
-import Content from './Content';
-import Footer from './Footer';
-import { useState } from 'react';
+// import Header from './Header';
+// import SearchItem from './SearchItem';
+// import AddItem from './AddItem';
+// import Content from './Content';
+// import Footer from './Footer';
+// import { useState } from 'react';
+
+// function App() {
+//   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+//   const [newItem, setNewItem] = useState('')
+//   const [search, setSearch] = useState('')
+
+//   const setAndSaveItems = (newItems) => {
+//     setItems(newItems);
+//     localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+//   }
+
+//   const addItem = (item) => {
+//     const id = items.length ? items[items.length - 1].id + 1 : 1;
+//     const myNewItem = { id, checked: false, item };
+//     const listItems = [...items, myNewItem];
+//     setAndSaveItems(listItems);
+//   }
+
+//   const handleCheck = (id) => {
+//     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+//     setAndSaveItems(listItems);
+//   }
+
+//   const handleDelete = (id) => {
+//     const listItems = items.filter((item) => item.id !== id);
+//     setAndSaveItems(listItems);
+//   }
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!newItem) return;
+//     addItem(newItem);
+//     setNewItem('');
+//   }
+
+//   return (
+//     <div className="App">
+//       <Header title="Grocery List" />
+//       <AddItem
+//         newItem={newItem} 
+//         setNewItem={setNewItem}
+//         handleSubmit={handleSubmit}
+//       />
+//       <SearchItem
+//         search={search}
+//         setSearch={setSearch}
+//       />
+//       <Content
+//         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+//         handleCheck={handleCheck}
+//         handleDelete={handleDelete}
+//       />
+//       <Footer length={items.length} />
+//     </div>
+//   );
+// }
+
+import Square from "./Square";
+import Input from "./Input"
+import {useState} from 'react'
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const [colorValue, setColorValue] = useState('')
+  const [hexValue, setHexValue] = useState('')
+  const [isDarkText, setIsDarkText] = useState(true)
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-  }
-
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item };
-    const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-  }
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setAndSaveItems(listItems);
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem('');
-  }
-
-  return (
+  return(
     <div className="App">
-      <Header title="Grocery List" />
-      <AddItem
-        newItem={newItem} 
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
-      <Footer length={items.length} />
+      <Square 
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}/>
+      <Input 
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}/>
     </div>
   );
 }
-
 export default App;
