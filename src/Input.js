8f57cd181d0401d103028cced86240124ae55d25
Jakub Baczyński <124045523+JakubBaczynskii@users.noreply.github.com>
2025-03-06 13:41:17 +0100

    Ending Lesson

--- src/Input.js
+++ src/Input.js
@@ -1,19 +1,25 @@
 import colorNames from "colornames";
+import {useRef} from 'react'
 
 const Input = ({ colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText}) => {
+    const inputRef = useRef()
+
+    const handleChange = () => {
+        const newValue = inputRef.current.value;
+        setColorValue(newValue);
+        setHexValue(colorNames(newValue));
+    };
     return(
         <form onSubmit={(e) => e.preventDefault()}>
             <label>Add Color Name:</label>
             <input 
+                ref={inputRef}
                 autoFocus
                 type="text"
                 placeholder="Add color name"
                 required
                 value={colorValue}
-                onChange={(e) => {
-                    setColorValue(e.target.value);
-                    setHexValue(colorNames(e.target.value))
-                }}/>
+                onChange={handleChange}/>
                 <button 
                     type="button"
                     onClick={() => setIsDarkText(!isDarkText)}
